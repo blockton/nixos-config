78d3bffcfdf3aca41a41778bab0c575f15189ab2
blockton <w.mohammed.6659@gmail.com>
2025-10-07 17:15:42 +0300

    add refined github to chromium extensions

--- home/programs/chromium/default.nix
+++ home/programs/chromium/default.nix
@@ -8,6 +8,7 @@
       "gighmmpiobklfepjocnamgkkbiglidom" # ad blocker
       "eimadpbcbfnmbkopoojfekhnkhdbieeh" # dark reader
       "bggfcpfjbdkhfhfmkjpbhnkhnpjjeomc" # github icons
+      "hlepfoohegkhhmjieoechaddaejaokhf" # refined github
       "aapbdbdomjkkjkaonfhkkikfgjllcleb" # google translate
     ];
   };
