dfc1f751dde9d0b1bf01f4148c9334a47854d285
blockton <blockton.dev@gmail.com>
2026-01-22 18:13:21 +0300

    feat(vscodeium): add some new extensions

--- modules/home/programs/vscodium/extensions.nix
+++ modules/home/programs/vscodium/extensions.nix
@@ -1,15 +1,37 @@
 { pkgs, ... }:
 let
-  jonathanharty.gruvbox-material-icon-theme =
-    pkgs.vscode-utils.buildVscodeMarketplaceExtension
-      {
-        mktplcRef = {
-          name = "gruvbox-material-icon-theme";
-          publisher = "JonathanHarty";
-          version = "1.1.5";
-          hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
-        };
-      };
+  natizyskunk.sftp = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "sftp";
+      hash = "sha256-HifPiHIbgsfTldIeN9HaVKGk/ujaZbjHMiLAza/o6J4=";
+      version = "1.16.3";
+      publisher = "natizyskunk";
+    };
+  };
+  ionutvmi.path-autocomplete = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "path-autocomplete";
+      hash = "sha256-iz32o1znwKpbJSdrDYf+GDPC++uGvsCdUuGaQu6AWEo=";
+      version = "1.25.0";
+      publisher = "ionutvmi";
+    };
+  };
+  tldraw-org.tldraw-vscode = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "tldraw-vscode";
+      hash = "sha256-h5OpIH4l2QJonb5pk5RxGYPiSX/zPXPs6OZPZi6Dor8=";
+      version = "2.199.1";
+      publisher = "tldraw-org";
+    };
+  };
+  jonathanharty.gruvbox-material-icon-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "gruvbox-material-icon-theme";
+      hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
+      version = "1.1.5";
+      publisher = "JonathanHarty";
+    };
+  };
 in
 {
   programs.vscode.profiles.default = {
@@ -26,7 +48,11 @@ in
       arrterian.nix-env-selector
 
       # Tools
+      natizyskunk.sftp
+      tldraw-org.tldraw-vscode
       wakatime.vscode-wakatime
+      ionutvmi.path-autocomplete
+
       # Formatters
       esbenp.prettier-vscode
 
